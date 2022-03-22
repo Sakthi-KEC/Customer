a032a0ac8989284490c46602cd1399679a66c180
Sakthi-KEC <97332413+Sakthi-KEC@users.noreply.github.com>
2022-03-22 18:41:15 +0530

    Update CustomerController.java

--- src/main/java/net/microservices/Customer/controller/CustomerController.java
+++ src/main/java/net/microservices/Customer/controller/CustomerController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/customer")
 
 public class CustomerController
